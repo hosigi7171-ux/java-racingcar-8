3cfa17abe2693416302f285b452607864b7bcc44
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-22 17:02:47 +0900

    test(RacingCar): 자동차 전진이 올바르게 작동할지 테스트 코드 작성
    자동차가 전진하면 전진거리가 갱신되는지, 숫자에 따라 전진 혹은 제자리가 제대로 되는지 확인하는 테스트 코드 작성

---
+++ src/main/java/racingcar/RacingCar.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class RacingCar {
+    private String name;
+    private int forwardDistance;
+
+    public RacingCar(String testDriver) {
+    }
+
+    public void moveIfAtLeastFour(int i) {
+    }
+
+    public int getForwardDistance() {
+        return forwardDistance;
+    }
+
+    public void setForwardDistance(int forwardDistance) {
+        this.forwardDistance = forwardDistance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/racingcar/RacingCarTest.java
@@ -0,0 +1,43 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class RacingCarTest {
+    private RacingCar car;
+
+    @BeforeEach
+    void 자동차_객체_세팅() {
+        car = new RacingCar("testDriver");
+    }
+
+    @Test
+    void 자동차가_전진시_전진거리_증가한다() {
+        car.moveIfAtLeastFour(4);
+
+        assertThat(car.getForwardDistance()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 자동차가_4이상의_숫자를_받으면_전진한다() {
+        car.moveIfAtLeastFour(4);
+
+        assertThat(car.getForwardDistance()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4})
+    void 자동차가_4미만의_숫자를_받으면_전진하지_않는다() {
+        car.moveIfAtLeastFour(3);
+
+        assertThat(car.getForwardDistance()).isEqualTo(0);
+    }
+}
