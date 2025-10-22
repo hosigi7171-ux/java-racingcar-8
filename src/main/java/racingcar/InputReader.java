95114315689ea7a6687cceb2204639d08e4ff45a
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-22 22:10:18 +0900

    test(InputReader): InputReader 테스트 코드 작성

---
+++ src/main/java/racingcar/InputReader.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class InputReader {
+
+    public String readCarNames() {
+        return null;
+    }
+
+    public String readTryCount() {
+        return null;
+    }
+}
---
+++ src/test/java/racingcar/InputReaderTest.java
@@ -0,0 +1,39 @@
+package racingcar;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InputReaderTest {
+    private InputReader inputReader;
+
+    @BeforeEach
+    void setup() {
+        inputReader = new InputReader();
+    }
+
+    @Test
+    void 자동차_이름들을_올바르게_입력받는지_검증() {
+        String input = "car1,car2,car3";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        String result = inputReader.readCarNames();
+
+        assertThat(result).isEqualTo(input);
+    }
+
+    @Test
+    void 시도횟수를_올바르게_입력받는지_검증() {
+        String input = "5";
+        InputStream in = new ByteArrayInputStream((input.getBytes()));
+        System.setIn(in);
+
+        String result = inputReader.readTryCount();
+
+        assertThat(result).isEqualTo(input);
+    }
+}
