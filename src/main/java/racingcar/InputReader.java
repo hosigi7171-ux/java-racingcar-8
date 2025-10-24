f8f6f79547eca567187a0aa1f41ab9fea77fb2bc
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-24 10:11:33 +0900

    test(InputReader): temporarily comment out Console.close() for ApplicationTest compatibility
    - Calling Console.close() during tests closes input stream and causes errors
    - Temporarily disabled for test stability
    - Will be restored after fixing test input handling

--- src/main/java/racingcar/InputReader.java
+++ src/main/java/racingcar/InputReader.java
@@ -3,7 +3,7 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputReader {
-    private InputValidator validator;
+    private final InputValidator validator;
 
     public InputReader(){
         validator = new InputValidator();
@@ -12,7 +12,7 @@ public class InputReader {
     public String readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesString = Console.readLine();
-        Console.close(); // 외부 라이브러리 Console scanner 초기화 용도
+//        Console.close(); // 외부 라이브러리 Console scanner 초기화 용도
 
         validator.validateCarNames(carNamesString);
 
@@ -22,7 +22,7 @@ public class InputReader {
     public String readTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCountString = Console.readLine();
-        Console.close();  // 외부 라이브러리 Console scanner 초기화 용도
+//        Console.close();  // 외부 라이브러리 Console scanner 초기화 용도
 
         validator.validateTryCount(tryCountString);
 
